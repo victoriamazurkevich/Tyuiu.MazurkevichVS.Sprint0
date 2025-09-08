e197ab4252252ee0a73608e84d97ce065f3735a4
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-09-08 22:16:05 +0500

    Task2

---
+++ Tyuiu.MazurkevichVS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MazurkevichVS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MazurkevichVS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void СheckGetMessageValid()
+        {
+            var name = "Вика";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Вика", res);
+        }
+    }
+}
---
+++ Tyuiu.MazurkevichVS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MazurkevichVS.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
