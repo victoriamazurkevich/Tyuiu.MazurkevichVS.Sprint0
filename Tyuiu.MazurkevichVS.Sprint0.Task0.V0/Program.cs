a294bbe632a05e49e19ae3192ef4361ea2cedcd0
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-09-08 21:57:15 +0500

    has been edited

--- Tyuiu.MazurkevichVS.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.MazurkevichVS.Sprint0.Task0.V0/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Tyuiu.MazurkevichVS.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Tyuiu.MazurkevichVS.Sprint0.Task1,V0/Program.cs
+++ Tyuiu.MazurkevichVS.Sprint0.Task1,V0/Program.cs
@@ -1,16 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Tyuiu.MazurkevichVS.Sprint0.Task1.V0
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World");
             string name = "Вика";
             int age = 18;
             Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет!");
