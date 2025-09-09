dddb8b17c3a31e1debecdf1d078ad168aebf75d5
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-09-09 14:51:49 +0500

    Task3

---
+++ Tyuiu.MazurkevichVS.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Tyuiu.MazurkevichVS.Sprint0.Task3.V0.Lib;
+{
+    Console.WriteLine(DataService.Sum(7, 7));
+    Console.ReadKey();
+}
