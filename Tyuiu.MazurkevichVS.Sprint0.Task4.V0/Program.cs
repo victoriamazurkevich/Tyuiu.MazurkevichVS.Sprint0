4eb0224d9f7a769824c2d6143ef0d3cdb1aae90c
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-09-12 19:55:11 +0500

    Task4

---
+++ Tyuiu.MazurkevichVS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.MazurkevichVS.Sprint0.Task4.V0.Lib;
+
+{
+    Console.WriteLine(DataService.Addition(1, 5));
+    Console.WriteLine(DataService.Subtraction(10, 5));
+    Console.WriteLine(DataService.Multiplication(2, 5));
+    Console.WriteLine(DataService.Division(15, 5));
+    Console.ReadKey();
+}
